76738cbff9663e09da271fae37160ac880607e96
Jack Rans <jwrans@gmail.com>
2016-11-28 09:47:36 +0000

    :bug: Only return if at least one id field present #12

--- test/sql_gen.test.js
+++ test/sql_gen.test.js
@@ -59,22 +59,40 @@ tape('::select - gen. SQL to select cols from table w/ where', function (t) {
   t.end();
 });
 
-tape('::insert - generate SQL to insert a column into a table', function (t) {
+tape('::select - gen. SQL to select from join', function (t) {
+  var query = sqlGen.select(null, {
+    innerJoin: {
+      table1: 'team',
+      table2: 'player',
+      column1: 'id',
+      column2: 'team_id'
+    }
+  });
+
+  t.equal(
+    query[0],
+    'SELECT * FROM "team" JOIN "player" ON "team"."id" = "player"."team_id"',
+    'Generate psql for JOIN'
+  );
+  t.end();
+});
+
   var query = sqlGen.insert(
-    { tableName: schema.tableName, fields: {} },
+    schema,
     { tableName: schema.tableName, fields: { email: 'me@poop.com' } }
   );
 
   t.equal(
     query[0],
-    'INSERT INTO "user_data" (email) VALUES ($1) RETURNING ()',
+    'INSERT INTO "user_data" (email, id) VALUES ($1, $2) RETURNING (id)',
     'Generate parameterised query'
   );
   t.deepEqual(
-    query[1],
-    ['me@poop.com'],
+    query[1][0],
+    'me@poop.com',
     'Generate values for parameterised query'
   );
+  t.ok(query[1][1], 'Id generated');
   t.end();
 });
 
@@ -86,7 +104,7 @@ tape('::insert - generate SQL to insert blank col into table', function (t) {
 
   t.equal(
     query[0],
-    'INSERT INTO "user_data" () VALUES () RETURNING ()',
+    'INSERT INTO "user_data" () VALUES ()',
     'Generate query for blank line'
   );
   t.deepEqual(
